b8028b15cfe7ad4edc0bc968a4af1ea00595a205
omerhof <fghg3HTL_wFKor4nFLmp>
2022-05-06 10:19:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+def load_data():
+    pass
+
+def explore_data():
+    pass
+
+def make_pairs2():
+    pass
+
+def create_architecture():
+    pass
+
+def configure_model():
+    pass
+
+def train_model():
+    pass
+
+def evaluate_model():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
